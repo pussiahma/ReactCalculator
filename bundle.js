6abb7a5e9375e98f7b13ba5a7057b7604bb54531
pussiahma <ella.ruokokoski@gmail.com>
2017-07-21 11:42:53 +0300

    update bundle.js

--- bundle.js
+++ bundle.js
@@ -24982,6 +24982,9 @@ function verifySubselectors(mapStateToProps, mapDispatchToProps, mergeProps, dis
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 exports.default = NumButtonReducer;
 
 var _numbutton = __webpack_require__(98);
@@ -25005,77 +25008,44 @@ function NumButtonReducer() {
     case NumButtonActionTypes.GET_NUMBER:
 
       if (state.number === '-0' || state.number === '.0' || state.number === '0' || state.number.endsWith('-0') || state.number.endsWith('/ 0') || state.number.endsWith('+ 0') || state.number.endsWith('* 0')) {
-        return {
-          state: state,
-          number: state.number,
-          numberArr: state.numberArr,
-          mathButtonArr: state.mathButtonArr
-        };
+        return _extends({}, state);
       }
 
-      return {
-        state: state,
-        number: state.number == '' ? action.text : state.number + action.text,
-        numberArr: state.numberArr,
-        mathButtonArr: state.mathButtonArr
-      };
+      return _extends({}, state, {
+        number: state.number == '' ? action.text : state.number + action.text
+      });
 
     case NumButtonActionTypes.GET_MATH_BUTTON:
       {
-        var contains = function contains(string, arr) {
-          var value = 0;
-          arr.forEach(function () {
-            value = value + string.includes(char);
-          });
-
-          return value === 1;
-        };
 
         var last = state.number.substr(state.number.length - 1);
         var end = state.number.substr(state.number.length - 3);
 
-        if (action.text === "." && contains(state.number, state.mathButtonArr) && state.number.includes(".")) {
-          return {
-            state: state,
-            number: state.number,
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+        if (action.text === '.') {
+          if (state.number.includes('.') && !end.includes(' ') || end.includes('.')) {
+            return _extends({}, state);
+          }
         }
 
-        if (action.text === '.' || action.text === ' -') {
+        if (action.text === ' -' || action.text === '.') {
           if (last === '.' || last === '-') {
-            return {
-              state: state,
-              number: state.number,
-              numberArr: state.numberArr,
-              mathButtonArr: state.mathButtonArr
-            };
+            return _extends({}, state);
           }
 
-          return {
-            state: state,
-            number: state.number + action.text,
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: state.number + action.text
+          });
         }
 
         if (last === "." || last === "-" || end === " / " || end === " * " || end === " + ") {
-          return {
-            state: state,
-            number: state.number,
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: state.number
+          });
         }
 
-        return {
-          state: state,
-          number: state.number === "" ? "" : state.number + action.text,
-          numberArr: state.numberArr,
-          mathButtonArr: state.mathButtonArr
-        };
+        return _extends({}, state, {
+          number: state.number === "" ? "" : state.number + action.text
+        });
       }
 
     case NumButtonActionTypes.GET_RESULT:
@@ -25085,57 +25055,39 @@ function NumButtonReducer() {
         var endChar = action.value.substr(action.value.length - 3);
 
         if (endChar === " + " || endChar === " / " || endChar === " * ") {
-          return {
-            state: state,
-            number: eval(action.value.substring(0, action.value.length - 3)).toString(),
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: eval(action.value.substring(0, action.value.length - 3)).toString()
+          });
         }
 
         if (lastChar === "." || lastChar === "-") {
 
-          return {
-            state: state,
-            number: eval(action.value.substring(0, action.value.length - 1)).toString(),
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: eval(action.value.substring(0, action.value.length - 1)).toString()
+          });
         }
 
         if (eval(action.value) % 1 === 0) {
-          return {
-            state: state,
-            number: eval(action.value).toString(),
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: eval(action.value).toString()
+          });
         }
 
         if (eval(action.value).toString().includes(".") && eval(action.value).toFixed(4).toString().endsWith("00")) {
-          return {
-            state: state,
-            number: eval(action.value).toFixed(2).toString(),
-            numberArr: state.numberArr,
-            mathButtonArr: state.mathButtonArr
-          };
+          return _extends({}, state, {
+            number: eval(action.value).toFixed(2).toString()
+          });
         }
 
-        return {
-          state: state,
-          number: eval(action.value).toFixed(4).toString(),
-          numberArr: state.numberArr,
-          mathButtonArr: state.mathButtonArr
-        };
+        return _extends({}, state, {
+          number: eval(action.value).toFixed(4).toString()
+        });
       }
 
     case NumButtonActionTypes.CLEAR:
-      return {
-        state: state,
-        number: '',
-        numberArr: state.numberArr,
-        mathButtonArr: state.mathButtonArr
-      };
+      return _extends({}, state, {
+        number: ''
+      });
 
     default:
       return state;
